9b3ad82114ccd74486beaeb335654feaa41d1a49
thomas-waite <thomas.waite4@gmail.com>
2020-08-23 18:42:59 +0100

    feat(protocol): add ropsten deploy config

--- packages/protocol/buidler.config.ts
+++ packages/protocol/buidler.config.ts
@@ -1,7 +1,10 @@
-import { BuidlerConfig, usePlugin } from '@nomiclabs/buidler/config';
+import { BuidlerConfig, usePlugin } from "@nomiclabs/buidler/config";
 import "./tasks/typechain";
+import dotenv from "dotenv";
 
-usePlugin('@nomiclabs/buidler-waffle');
+dotenv.config();
+
+usePlugin("@nomiclabs/buidler-waffle");
 usePlugin("solidity-coverage");
 
 const config: BuidlerConfig = {
@@ -11,14 +14,22 @@ const config: BuidlerConfig = {
       enabled: false,
       runs: 200,
     },
-    version: '0.6.10',
+    version: "0.6.10",
   },
   networks: {
     buidlerevm: {
       blockGasLimit: 10000000,
       gas: 8000000,
-      hardfork: 'istanbul',
+      hardfork: "istanbul",
       chainId: 31337,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      chainId: 3,
+      gas: 8000000,
+      from: process.env.TESTING_ACCOUNT_ADDRESS,
+      accounts: [process.env.TESTING_ACCOUNT_PRIV_KEY!, process.env.TESTING_ACCOUNT_PRIV_KEY_2!],
+      gasPrice: 10000000000,
     },
     coverage: {
       url: "http://127.0.0.1:8555",
