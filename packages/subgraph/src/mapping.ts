c08da9e925813f4db15a63c51491f5b80715e8b6
thomas-waite <thomas.waite4@gmail.com>
2020-08-23 17:49:33 +0100

    fix(subgraph): fix entity params

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -6,14 +6,13 @@ import {
   Dispute,
   Withdraw,
   DepositReleaseApproval,
-} from '../generated/BetterDeposits/Escrow';
+} from '../generated/BetterDeposits/BetterDeposits';
 import { Escrow } from '../generated/schema';
 
 export function handleNewEscrow(event: Create): void {
-  let escrow = new Escrow(event.params.id.toHex());
-  escrow.escrowId = event.params.escrowId;
-  escrow.userA = event.params.userA;
-  escrow.userB = event.params.userB;
+  let escrow = new Escrow(event.params.escrowId.toHex());
+  escrow.id = event.params.escrowId.toString();
+  escrow.user = event.params.userA;
+  escrow.counterParty = event.params.userB;
   escrow.save();
 }
-
