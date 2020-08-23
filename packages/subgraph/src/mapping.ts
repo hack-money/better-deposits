8ef2dc41ad312e78a0761fd96f7a3b038ea9b65f
thomas-waite <thomas.waite4@gmail.com>
2020-08-23 23:56:02 +0100

    feat(subgraph): add handlers for all events, update entity

--- packages/subgraph/src/mapping.ts
+++ packages/subgraph/src/mapping.ts
@@ -12,7 +12,37 @@ import { Escrow } from '../generated/schema';
 export function handleNewEscrow(event: Create): void {
   let escrow = new Escrow(event.params.escrowId.toHex());
   escrow.id = event.params.escrowId.toString();
-  escrow.user = event.params.userA;
-  escrow.counterParty = event.params.userB;
+  escrow.userA = event.params.userA;
+  escrow.userB = event.params.userB;
+  escrow.status = 'PRE-ACTIVE';
   escrow.save();
+}
+
+export function handleDeposit(event: Deposit): void {
+  const id = event.params.escrowId.toHex();
+  const escrow = Escrow.load(id)!;
+
+  if (event.params.depositAddress === escrow.userA) {
+    escrow.userADeposit = event.params.depositAmount;
+  } else if (event.params.depositAddress === escrow.userB) {
+    escrow.userBDeposit = event.params.depositAmount;
+  }
+}
+
+export function handleAgreementStart(event: AgreementStart): void {
+  const id = event.params.escrowId.toHex();
+  const escrow = Escrow.load(id)!;
+  escrow.status = 'STARTED';
+}
+
+export function handleAgreementFinish(event: AgreementFinish): void {
+  const id = event.params.escrowId.toHex();
+  const escrow = Escrow.load(id)!;
+  escrow.status = 'STARTED';
+}
+
+export function handleDispute(event: Dispute): void {
+  const id = event.params.escrowId.toHex();
+  const escrow = Escrow.load(id)!;
+  escrow.status = 'DISPUTE';
 }
